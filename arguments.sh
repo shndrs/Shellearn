c881d3588970f6c8d47d25ffdc6208c935e376a8
NP2 <sahandraeisi1994@gmail.com>
2019-07-28 17:15:00 +0430

    Added arguments example

--- arguments.sh
+++ arguments.sh
@@ -1,2 +1,13 @@
 #! /bin/bash
+# added $0 for seeing .sh file name
 
+echo $0 $1 $2   ' ---> echo $1 $2'
+
+# arguments in array
+
+args=("$@")
+
+echo ${args[0]} ${args[1]} # first argument whould be in index 0
+
+echo $@ # print hole array
+echo $# # number of arguments
