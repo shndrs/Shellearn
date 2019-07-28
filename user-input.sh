0041a2a8864750009f848d9c3017bc3d172d86f0
NP2 <sahandraeisi1994@gmail.com>
2019-07-28 15:25:53 +0430

    Create a login with username and password

---
+++ user-input.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# echo "Please enter your username: "
+# read -p username
+# echo "Please enter you password: "
+# read -p password
+
+# echo "username is : $username and password is : $password"
+
+# multiple inputs : read username password hint
+
+# -s secure input text
+# -p same line enterance
+# secure in same line : -sp OR -s -p
+
+read -p 'Enter your username: ' username
+read -s -p 'Enter your password: ' password
+
+echo "username is : $username, password is : $password "
