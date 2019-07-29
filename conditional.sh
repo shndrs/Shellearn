b546ecccd389298a855f9bf0489322259b89339f
NP2 <sahandraeisi1994@gmail.com>
2019-07-29 09:33:00 +0430

    Added a cool if statement example

--- conditional.sh
+++ conditional.sh
@@ -1,5 +1,51 @@
 #! /bin/bash
 
+count=10
+
+if [ $count -ne 9 ]
+then
+    echo "condition is true"
+fi
+
+word=shndrs
+ 
+if [ $word != "shnd" ]
+then
+    echo "is not equal"
+else
+    echo "is equal"
+fi
+
+if [[ $word == "sh" ]]
+then 
+    echo "sh is true"
+elif [[ $word == "shndrs" ]]
+then
+    echo "condition is shndrs"
+else    
+    echo "you're going to die :|"
+fi
+
+# now somthing a little bit cooler
+
+echo "Please enter your fullname : "
+
+read -a fullname
+
+if [[ ${fullname[0]} == "sahand" ]]
+then
+    echo "your firstname is Sahand, Cool! So am i"
+else
+    echo "your firstname is not equal to mine"
+fi
+
+if [[ ${fullname[1]} == "raeisi" ]]
+then 
+    echo "your lastname is Raeisi, Cool! So am i"
+else
+    echo "your lastname is not equal to mine"
+fi
+
 # INTEGER Comparison
 
 # if ["$a" -eq "$b"]
@@ -30,34 +76,3 @@
 # < is less than, in ASCII alphabetical order
 # > is greater than, in ASCII alphabetical order
 # -z string is null, has zero lenth
-
-
-count=10
-
-if [ $count -ne 9 ]
-then
-    echo "condition is true"
-fi
-
-word=shndrs
- 
-if [ $word != "shnd" ]
-then
-    echo "is not equal"
-else
-    echo "is equal"
-fi
-
-if [[ $word == "sh" ]]
-then 
-    echo "sh is true"
-elif [[ $word == "shndrs" ]]
-then
-    echo "condition is shndrs"
-else    
-    echo "you're going to die :|"
-fi
-
-# now somthing a little bit cooler
-
-echo "Please enter you full name"
