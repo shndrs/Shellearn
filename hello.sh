bfcfcb4f460dbba9ac9411ec4a04318d23d026d1
NP2 <sahandraeisi1994@gmail.com>
2019-07-29 08:40:44 +0430

    Added conditional.sh

--- hello.sh
+++ hello.sh
@@ -1,22 +1,2 @@
 #! /bin/bash
 echo "Hello World"
-
-# System Variable
-
-echo $BASH # give us name of the bash or shell
-echo $BASH_VERSION
-echo $HOME
-echo $PWD # present working directory
-echo $LANG
-echo $DESKTOP_SESSION
-echo $SESSION_MANAGER
-echo $SHELLOPTS
-echo $SHELL
-
-# User define variable
-
-account=shndrs
-echo My Github account name is $account
-
-value=666
-echo Your value is $value
