5e25f27226114ad4c3d6fac7e1846d739c9eb980
NP2 <sahandraeisi1994@gmail.com>
2019-07-31 14:20:00 +0430

    Added example to conditional-and-file-test-operator.sh

--- conditional-and-file-test-operator.sh
+++ conditional-and-file-test-operator.sh
@@ -1,2 +1,19 @@
 #! /bin/bash
 
+echo -e "Enter the file name : \c"
+read file_name
+
+# tip: > override the file >> append to the file
+
+if [ -f $file_name ]
+then
+    if [ -w $file_name ]
+    then
+        echo "Type your text data, and for Exit press ctrl+d"
+        cat >> $file_name
+    else
+        echo "The file do not have a write permissions"
+    fi
+else
+    echo "$file_name is not exist!"
+fi
