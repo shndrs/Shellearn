0197361db88f8abb373d4f2c3eea1a30d22e73ad
NP2 <sahandraeisi1994@gmail.com>
2019-07-31 15:52:42 +0430

    3 kind of AND

---
+++ and-operator.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo -e "Please enter your age for enterance validation : \c"
+read age
+
+# with one bracket [ "$age" -gt 18 -a "$age" -lt 30 ]
+# with two in row brackets [[ "$age" -gt 18 && "$age" -lt 30 ]]
+
+if [ "$age" -gt 18 ] && [ "$age" -lt 30 ]
+then
+    echo "The age is valid"
+else
+    echo "age is invalid"
+fi
