7189871387e1e0c585d54abbf2c4a73d6deaf112
Sahand Raeisi <sahandraeisi@yahoo.com>
2019-08-02 19:39:18 +0430

    Added arithmathic operation with (()) and expr keyword

--- arithmetics-operations.sh
+++ arithmetics-operations.sh
@@ -1,1 +1,23 @@
 #! /bin/bash
+
+num1=41
+num2=14
+
+# with (())
+
+echo $((num1 + num2))
+echo $((num1 - num2))
+echo $((num1 * num2))
+echo $((num1 / num2))
+echo $((num1 % num2))
+
+# with expr keyword
+
+echo "with expr : "
+
+echo $(expr $num1 + $num2 )
+echo $(expr $num1 - $num2 )
+# tip: \ is escaping character in multiplications
+echo $(expr $num1 \* $num2 )
+echo $(expr $num1 / $num2 )
+echo $(expr $num1 % $num2 )
