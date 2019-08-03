ba44b01ebe6f37bccb322929e01e4145b6987ae3
NP2 <sahandraeisi1994@gmail.com>
2019-08-03 17:25:10 +0430

    Added example to advanced-case-statement

--- advanced-case-statement.sh
+++ advanced-case-statement.sh
@@ -3,3 +3,17 @@
 echo -e "Enter some character : \c"
 read input
 
+#LANG=C language and locale encoding
+
+case $input in
+    [a-z] ) # small letter between a to z
+        echo "You entered a to z" ;;
+    [A-Z] )
+        echo "You entered A to Z" ;;
+    [0-9] )
+        echo "You entered 0 to 9" ;;
+    ? ) # any special character - 1 character
+        echo "you entered $input special character" ;;
+    * )
+     echo "Unknown input" ;;
+esac
