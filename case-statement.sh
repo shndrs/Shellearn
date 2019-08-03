8ec96ba3fd2659d27e9590df360ed2c480052f0f
NP2 <sahandraeisi1994@gmail.com>
2019-08-03 16:54:53 +0430

    Added example to case-statement.sh

--- case-statement.sh
+++ case-statement.sh
@@ -1,1 +1,16 @@
-#! /bin.bash
+#! /bin/bash
+
+vehicle=$1
+
+case $vehicle in
+    "Ferrari" )
+        echo "Ferrari Vettel and Leclerc" ;;
+    "RedBull" )
+        echo "Verstappen and Gasly" ;;
+    "Mercedes" )
+        echo "Hamilton and Bottas" ;;
+    "Alfa Romeo" )
+        echo "Raikonnen and, Giovinazi" ;;
+    * )
+        echo "Unknown Formula 1 team" ;;
+esac
