e77c60299aa1b32334dd563a0155a183a36ddebf
NP2 <sahandraeisi1994@gmail.com>
2019-08-04 15:24:08 +0430

    Example added to for-loops.sh

--- for-loops.sh
+++ for-loops.sh
@@ -1,1 +1,21 @@
 #! /bin/bash
+
+# type one
+echo ${BASH_VERSION}
+for i in 1 2 3 4
+do
+    echo $i
+done
+
+for j in {1..10} # {start..end..increment} incerement support in bash 4 or higher
+do
+    echo $j
+done
+
+echo -e "Enter a number : \c"
+read num
+
+for ((k=0;k<=10;k++))
+do
+    echo "$num x $k = `expr $num \* $k`"
+done
