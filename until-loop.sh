355df00eb32c4918c771679c091696d1ff4a442a
NP2 <sahandraeisi1994@gmail.com>
2019-08-04 14:48:13 +0430

    Added counter example for until-loop.sh

--- until-loop.sh
+++ until-loop.sh
@@ -1,1 +1,13 @@
 #! /bin/bash
+
+no=1
+
+echo -e "Hey Counter \m/, please count to : (enter an integer) : \c"
+read input
+
+until [ $no -gt $input ]
+do
+    echo $no
+    (( ++no ))
+    sleep 0.4
+done
