4b0a4a185632182dc6c0779990b7fcf24cf8ce67
NP2 <sahandraeisi1994@gmail.com>
2019-08-04 12:06:36 +0430

    Added examples to while-loop.sh

--- while-loop.sh
+++ while-loop.sh
@@ -1,2 +1,25 @@
 #! /bin/bash
 
+value=0
+
+while [ $value -le 10 ] # (( $value <= 10 ))
+do
+    echo "$value"
+    value=$(( value+1 )) # 1. (( value+1 )) 2. (( value++ )) 3. (( ++value ))
+done
+
+val=1000
+
+while [ $val -le 100000 ]
+do
+    echo "$val"
+    val=$(( val*3 ))
+done
+
+var=300
+
+while (( $var < 310 ))
+do
+    echo "$var"
+    (( ++var ))
+done
