3f0972ced520956f689a5e1fe550d6a32e777df8
NP2 <sahandraeisi1994@gmail.com>
2019-08-05 14:48:20 +0430

    Added break and continue example

--- break-continue.sh
+++ break-continue.sh
@@ -1,1 +1,23 @@
 #! /bin/bash
+
+echo "-------------BREAK---------------"
+
+for (( i=1 ; i<= 10 ; i++ ))
+do
+    if [ $i -gt 5 ]
+    then
+        break
+    fi
+    echo "$i"
+done
+
+echo "------------CONTINUE------------"
+
+for (( i=1 ; i<= 10 ; i++ ))
+do
+    if [ $i -eq 3 -o $i -eq 6 ]
+    then
+        continue
+    fi
+    echo "$i"
+done
