0f36df9b2c60c9a1e9925cef18651d1a19d493c4
NP2 <sahandraeisi1994@gmail.com>
2019-08-05 14:50:17 +0430

    Added Formula One number of team championships

--- select-loop.sh
+++ select-loop.sh
@@ -5,18 +5,23 @@ do
     case $f1_teams in
         Ferrari )
             echo "$f1_teams is 16 times F1 champion"
+            break
             ;;
         Redbull )
             echo "$f1_teams is 4 times F1 champion"
+            break
             ;;
         Mercedes )
             echo "$f1_teams is 11 times F1 champions"
+            break
             ;;
         AlfaRomeo )
             echo "$f1_teams is 2 times F1 champions"
+            break
             ;;
         Renault )
             echo "$f1_teams is 11 times F1 champions"
+            break
             ;;
         *)
             echo "Please choose a number between 1 to 5."
