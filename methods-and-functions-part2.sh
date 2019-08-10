594a18792846a85f9a9c441681912c55ba959df5
NP2 <sahandraeisi1994@gmail.com>
2019-08-10 09:19:08 +0430

    Ternary operation example added

--- methods-and-functions-part2.sh
+++ methods-and-functions-part2.sh
@@ -1,2 +1,23 @@
 #! /bin/bash
 
+usage() {
+    echo "You need to provide an argument : "
+    echo "usage : $0 file_name"
+}
+
+
+is_file_exist() {
+    local file="$1"
+    [[ -f "$file" ]] && return 0 || return 1 # ternary operation
+}
+
+# $# is number of argument
+
+[[ $# -eq 0 ]] && usage
+
+if ( is_file_exist "$1" )
+then
+    echo "File found"
+else
+    echo "File not found"
+fi
