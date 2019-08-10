6a687a0c73d1457452a64b520338dde01b896a39
NP2 <sahandraeisi1994@gmail.com>
2019-08-10 09:36:08 +0430

    Added example to the read-only-command

--- read-only-command.sh
+++ read-only-command.sh
@@ -12,8 +12,12 @@ kill() {
     echo "Kill'em All"
 }
 
-readonly -f hello
+readonly -f kill # in function use we shod put -f behind the function
 
 kill() {
     echo "Don't kill'em all"
 }
+
+readonly -p # give us list of all readonly variabless
+echo "==============================="
+readonly -f # give us list of all readonly funcs
